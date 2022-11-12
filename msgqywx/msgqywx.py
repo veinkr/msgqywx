8a14e3aa591b914f862fda7394f96860e7f9073f
veinkr <yhf2lj@gmail.com>
2022-11-12 20:29:36 +0800

    update parameter

--- msgqywx/msgqywx.py
+++ msgqywx/msgqywx.py
@@ -16,7 +16,7 @@ class UserError(Exception):
 
 
 class msgqywx:
-    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+    url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
 
     def __init__(self, corpid: str, corpsecret: str, agentid: str, touser: str = None):
         """
@@ -30,32 +30,41 @@ class msgqywx:
         self.corpsecret = corpsecret
         self.agentid = agentid
         self.touser = touser
-        self.secret_key_md5 = md5((self.corpid + self.corpsecret).encode(encoding='utf-8')).hexdigest()
+        self.secret_key_md5 = md5(
+            (self.corpid + self.corpsecret).encode(encoding="utf-8")
+        ).hexdigest()
         self.base_config_folder = f"{os.path.expanduser('~')}/.config/msgqywx"
         if not os.path.exists(self.base_config_folder):
             os.makedirs(self.base_config_folder)
-        self.access_conf_file = os.path.join(self.base_config_folder, f"{self.secret_key_md5}.conf")
+        self.access_conf_file = os.path.join(
+            self.base_config_folder, f"{self.secret_key_md5}.conf"
+        )
 
     def access_token_to_jsonfile(self):
 
-        values = {'corpid': self.corpid,
-                  'corpsecret': self.corpsecret,
-                  }
+        values = {
+            "corpid": self.corpid,
+            "corpsecret": self.corpsecret,
+        }
         response = requests.post(self.url, params=values)
         if response.ok:
             data = json.loads(response.text)
-            secret_json = {"expire_time": datetime.now().timestamp() + 3600,
-                           "access_token": data["access_token"]}
-            with open(self.access_conf_file, 'w') as f:
+            secret_json = {
+                "expire_time": datetime.now().timestamp() + 3600,
+                "access_token": data["access_token"],
+            }
+            with open(self.access_conf_file, "w") as f:
                 json.dump(secret_json, f)
             return secret_json
         else:
             print(response.text)
-            raise Exception("获取企业微信的access_token失败,请检查企业ID（corpid）和应用Secret（corpsecret）是否正确")
+            raise Exception(
+                "获取企业微信的access_token失败,请检查企业ID（corpid）和应用Secret（corpsecret）是否正确"
+            )
 
     def get_access_token(self):
         if os.path.exists(self.access_conf_file):
-            with open(self.access_conf_file, 'r') as f:
+            with open(self.access_conf_file, "r") as f:
                 access_json = json.load(f)
                 if access_json.get("expire_time", 0) > datetime.now().timestamp():
                     return access_json["access_token"]
@@ -65,7 +74,13 @@ class msgqywx:
         else:
             return self.access_token_to_jsonfile()["access_token"]
 
-    def send_msg(self, message, msgtype: str = 'text', touser: str = None, raise_error: bool = False):
+    def send_msg(
+        self,
+        message,
+        msgtype: str = "text",
+        touser: str = None,
+        raise_error: bool = False,
+    ):
         """
         send_msg发送文本类消息
         :param msgtype: 消息类型，仅支持 text 和 markdown
@@ -74,23 +89,27 @@ class msgqywx:
         :param touser: 发送用户，和初始化类时的touser不能同时为None
         :return: 微信返回的response，可以自行处理错误信息，也可不处理
         """
-        if msgtype not in ['text', 'markdown']:
-            raise TypeError("Unsupported msgtype, only `text` and `markdown` are acceptable")
+        if msgtype not in ["text", "markdown"]:
+            raise TypeError(
+                "Unsupported msgtype, only `text` and `markdown` are acceptable"
+            )
 
-        touser = touser if touser else self.touser # use or
+        touser = touser if touser else self.touser  # use or
         if touser is None:
             raise UserError("touser must not None")
 
-        send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + \
-                   self.get_access_token() # use f-string
-        send_values = { # change send_values to payload
+        send_url = (
+            "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
+            + self.get_access_token()
+        )  # use f-string
+        payload = {
             "touser": touser,
             "agentid": self.agentid,
             "msgtype": msgtype,
-            msgtype: {"content": message}
+            msgtype: {"content": message},
         }
 
-        response = requests.post(send_url, json=send_values)
+        response = requests.post(send_url, json=payload)
         if not response.ok and raise_error:
             response.raise_for_status()
         else:
