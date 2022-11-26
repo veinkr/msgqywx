04fba806f2f74dc4181adfe8fa4c98f913f7d335
veinkr <yhf2lj@gmail.com>
2022-11-26 16:37:19 +0800

    feishu 增加 retry

--- msgqywx/feishu.py
+++ msgqywx/feishu.py
@@ -4,14 +4,18 @@ import requests
 class feishu:
     BASE_URL = "https://open.feishu.cn/open-apis/bot/v2/hook/"
 
-    def __init__(self, hook_id):
+    def __init__(self, hook_id, retry=1):
         self.url = self.BASE_URL + hook_id
+        self.retry = retry
 
-    def send(self, message: str, msgtype: str = "text", raise_error=False):
-        response = requests.post(
-            url=self.url, json={"msg_type": msgtype, "content": {"text": message}}
-        )
-        if not response.ok and raise_error:
-            response.raise_for_status()
+    def send(self, message: str, msgtype: str = "text", retry=None, raise_error=False):
+        retry = retry or self.retry
+        for i in range(retry):
+            response = requests.post(
+                url=self.url, json={"msg_type": msgtype, "content": {"text": message}}
+            )
+            if response.ok:
+                return response
         else:
-            return response
+            if raise_error:
+                response.raise_for_status()
